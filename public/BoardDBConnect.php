1cf3d2603fa7e204bb02a866ac1dd54ecb84674c
root <indosm@daum.net>
2020-07-01 21:23:26 +0900

    add specification

--- public/BoardDBConnect.php
+++ public/BoardDBConnect.php
@@ -1,5 +1,5 @@
 <?php
-	$conn = pg_connect("host=project-postgres dbname=db user=test password=test");
+	$conn = pg_connect("host=project-postgres dbname=db user=test password=test"); // host : docker container name <fixed_value>
 	if(!$conn){
 		die("Connection Error");
 	}
